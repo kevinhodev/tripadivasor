9a85b960abdcb495565366e57d3c4369babc40ef
Kevin Ramos Dos Santos De Oliveira <kevram52@outlook.com>
2022-01-23 22:09:47 -0300

    Projeto tripadivasor

---
+++ src/api/index.js
@@ -0,0 +1,23 @@
+import axios from "axios";
+
+export const getPlacesData = async (type, sw, ne) => {
+    try {
+        const { data: { data } } = await axios.get(`https://travel-advisor.p.rapidapi.com/${type}/list-in-boundary`, {
+          params: {
+            bl_latitude: sw.lat,
+            tr_latitude: ne.lat,
+            bl_longitude: sw.lng,
+            tr_longitude: ne.lng,
+            lang: 'pt_BR'
+          },
+          headers: {
+            'x-rapidapi-host': 'travel-advisor.p.rapidapi.com',
+            'x-rapidapi-key': '74ab7bf56dmsh541eebd0408c2d5p16ddb9jsne99869b3f89b'
+          }
+        });
+        return data;
+    }
+    catch (error) {
+        console.log(error);
+    }
+};
